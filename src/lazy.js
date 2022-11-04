ee09627bf1c33cae3abae248622817ee0bb97f8c
Flexx <juandavidx40@gmail.com>
2022-11-04 17:39:16 -0500

    Update files, add [Lazy Loading]

--- src/lazy.js
+++ src/lazy.js
@@ -1,21 +1,33 @@
-const isIntersecting = (entry) =>{
+const isIntersecting = entry => {
 
-    return entry.isIntersecting // true (dentro de la pantalla)
-};
+    return entry.isIntersecting;
 
-const accion = (entry) => {
-    const nodo = entry.target
-    console.log("holis");
-
-    //des registra la imagen(unlisten) sirve para que deje de registrar una vez ya registre las imagenes
-    observer.unobserve(nodo);
 }
 
-const observer = new IntersectionObserver((entries) => {
-    entries.filter(isIntersecting).forEach(accion)
+const loadImage = entry => {
+
+    const container = entry.target;
+    const imagen = container.querySelector("img");
+    const url = imagen.dataset.src;
+    imagen.src = url;
+
+    loadedImages++;
+    printLog();
+
+    observer.unobserve(container)
+
+}
+
+const observer = new IntersectionObserver(entries => {
+
+    entries
+        .filter(isIntersecting)
+        .forEach(loadImage);
+
 });
 
-export const registerImage = (img) => {
-// intersectactionOberserver -> observer(imagen)
-    observer.observe(img)
-};
+export const registerImage = imagen => {
+
+    observer.observe(imagen);
+
+}
